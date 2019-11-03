19974056342d916a3c554f0e1686733103c0e350
Jeevan-Thukrul <jeevanthukrul1438@gmail.com>
2019-11-03 19:42:48 +0530

    Function for adding order to database hardcoded values.2

--- app/src/main/java/com/example/tenderfarm1/Detect_order.java
+++ app/src/main/java/com/example/tenderfarm1/Detect_order.java
@@ -4,11 +4,45 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Detect_order extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detect_order);
+
+        String product_name="";
+        String product_id="";
+        String product_price="";
+        int inc=1;
+
+        FirebaseDatabase database=FirebaseDatabase.getInstance();
+
+        DatabaseReference myref=database.getReference("Orders");
+        DatabaseReference ch;
+/*
+        check_order(disease);
+        ch=myref.child("order"+inc);
+        ch.child("product_name").setValue(product_name);
+        ch.child("product_id").setValue(product_id);
+        ch.child("product_price").setValue(product_price);
+        inc++;
+
+    }
+    public String check_order(String t){
+        if (t =="a") {
+            product_name="aaa";
+            product_id="111";
+            product_price="999";
+        } else if(t=="b")
+        {
+            product_name="bbb";
+            product_id="222";
+            product_price="888";
+        }
+        return product_name, product_id,product_price;*/
     }
 }
